f1dcd2a5cd17cf73adc174e443b43a2ab7adfb81
schultyy <janschulte90@googlemail.com>
2013-10-19 17:15:59 +0200

    Replaced bootstrap.css with current version. Can render categories

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
 $(function(){
   var Keyword = Backbone.Model.extend({});
+  var Category = Backbone.Model.extend({});
 
   var KeywordCollection = Backbone.Collection.extend({
     model: Keyword
+  });
+
+  var CategoryCollection = Backbone.Collection.extend({
+    model: Category
   });
 
   var KeywordView = Backbone.View.extend({
     tagName: 'div',
     render: function(){
       var el = $(this.el);
-      el.append(this.model.get('name'));
+      var text = $('<p>').text(this.model.get('name'))
+                          .addClass('keyword');
+      el.append(text);
       return this;
     }
   });
+
   var SidebarView = Backbone.View.extend({
     el: $('#sidebar'),
     render: function(){
@@ -21,13 +29,45 @@ $(function(){
 
       this.collection.forEach(function(elem){
         var childView = new KeywordView({model: elem});
-        el.append(childView.el);
+        el.append(childView.render().el);
       });
       return this;
     }
   });
 
-  var keywords = new KeywordCollection();
-  var sidebar = new SidebarView({collection: keywords}).render();
+  var BentoBoxColumn = Backbone.View.extend({
+    tagName: 'div',
+    className: 'col-md-2',
+    render: function(){
+      var heading = $('<p>').addClass('BentoHeader')
+                            .text(this.model.get('name'));
+      $(this.el).append(heading);
+      return this;
+    }
+  });
 
+  var BentoBoxView = Backbone.View.extend({
+    el: $('#bentobox'),
+    render: function(){
+      var el = $(this.el);
+      this.collection.forEach(function(model){
+        var renderedColumn = new BentoBoxColumn({model: model}).render();
+        el.append(renderedColumn.el);
+      });
+    }
+  });
+
+  var keywords = new KeywordCollection();
+  var categories = new CategoryCollection();
+
+  var k1 = new Keyword({name: "foo"});
+  var cat1 = new Category({name: "Category 1"});
+  var cat2 = new Category({name: "Category 2"});
+
+  keywords.add(k1);
+  categories.add(cat1);
+  categories.add(cat2);
+
+  var sidebar = new SidebarView({collection: keywords}).render();
+  var bentobox = new BentoBoxView({collection: categories}).render();
 });
